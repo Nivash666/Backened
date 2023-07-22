57ae7254657a470b157644ef59d9825dd06905fa
Nivash666 <106662616+Nivash666@users.noreply.github.com>
2023-07-22 13:27:04 +0530

    commit-25

--- app_1/urls.py
+++ app_1/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("hello/",views.Hello.as_view(),name="hello"),
+    path("hello/",views.MyProtectedView.as_view(),name="hello"),
    #path("list_datas/",views.Shops.as_view(),name="listdatas"),
    #path("allshops/",views.list_all_products,name="allshops"),
 ]
