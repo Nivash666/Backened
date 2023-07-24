9b02afbcf893405f8509f59bbfd0913b1bbfce73
Nivash666 <106662616+Nivash666@users.noreply.github.com>
2023-07-24 11:13:58 +0530

    Commit-5

--- app_1/urls.py
+++ app_1/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("hello/",views.MyProtectedView.as_view(),name="hello"),
-    #path("store/",views.store_values,name="store"),
    #path("list_datas/",views.Shops.as_view(),name="listdatas"),
    #path("allshops/",views.list_all_products,name="allshops"),
 ]
--- app_1/views.py
+++ app_1/views.py
@@ -38,8 +38,6 @@ class MyProtectedView(ListAPIView):
 
 
 
-
-
 #from django.http import JsonResponse
 #from django.contrib.auth.decorators import login_required
 #
