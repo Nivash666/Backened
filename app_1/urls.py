3d9785bb5217b128b811b70a7c5847887651b08e
Nivash666 <106662616+Nivash666@users.noreply.github.com>
2023-08-07 20:32:49 +0530

    Commit-85

--- app_1/urls.py
+++ app_1/urls.py
@@ -3,9 +3,7 @@ from . import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
- 
+   
     path("hello/",views.MyProtectedView.as_view(),name="hello"),
     path("retriveshop/<str:pk>",views.retiveShop.as_view(),name="retriveshop"),
     path("postcart/",views.CartView.as_view(),name="postcart"),
