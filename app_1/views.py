31cf84e69b17fe3a8c625ddc453f9434790c6cbc
Nivash666 <106662616+Nivash666@users.noreply.github.com>
2023-08-09 21:51:15 +0530

    Commit-119

--- app_1/views.py
+++ app_1/views.py
@@ -31,13 +31,11 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
 def protected_view(request):
-    user = request.user
-    user_id = user.username  # or any other relevant attribute from the user object
+    auth_header = request.META.get('HTTP_AUTHORIZATION', '')
+    print(auth_header)
 
-    return Response({'message': 'Authenticated', 'user_id': user_id})
+    return Response({'message': 'Authenticated'})
 
 class MyProtectedView(APIView):
     def get(self, request, *args, **kwargs):
