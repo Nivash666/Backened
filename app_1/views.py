bc9221aab269eac11006db94a4e63f540f962c92
Nivash666 <106662616+Nivash666@users.noreply.github.com>
2023-08-12 09:47:15 +0530

    Commit-131

--- app_1/views.py
+++ app_1/views.py
@@ -42,7 +42,7 @@ from django.views import View
 class ProtectedView(APIView):
     def get(self, request, *args, **kwargs):
         auth_header = request.META.get('HTTP_AUTHORIZATION', None) or  request.META.get('Authorization')
-        print('Request-meta :'+ request.META)
+        print(request.META)
         if not auth_header:
             return JsonResponse({"message": "Authorization header missing"}, status=401)
 
