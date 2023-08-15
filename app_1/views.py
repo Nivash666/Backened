094dfe52d793ea1f572ccde6f281223f162b02ce
Nivash666 <106662616+Nivash666@users.noreply.github.com>
2023-08-15 11:26:10 +0530

    Commit-153

--- app_1/views.py
+++ app_1/views.py
@@ -53,7 +53,7 @@ class MyProtectedView(ListAPIView):
 
     def list(self, request,*args,**kwargs):
         queryset=self.get_queryset()
-        print(request.username)
+        print(request.user)
         serializer=self.get_serializer(queryset,many=True)
         return Response(serializer.data)    
 
