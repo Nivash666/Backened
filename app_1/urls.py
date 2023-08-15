c0e37596b0992db1cc046f5b5ae7c790d1fdc9d3
Nivash666 <106662616+Nivash666@users.noreply.github.com>
2023-08-15 13:11:32 +0530

    Commit-155

--- app_1/urls.py
+++ app_1/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("retriveshop/<str:pk>",views.retiveShop.as_view(),name="retriveshop"),
     path("postcart/",views.CartView.as_view(),name="postcart"),
     path("retrivecart/<str:pk>",views.RetriveCartShop.as_view(),name="retrivecart"),
-    path("protected/",views.Protectedview.as_view(),name="protected"),
+    path("protected/",views.MyProtectedView.as_view(),name="protected"),
 
    #path("list_datas/",views.Shops.as_view(),name="listdatas"),
    #path("allshops/",views.list_all_products,name="allshops"),
